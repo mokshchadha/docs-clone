ebdc9ccf6c481b54799f7f1b6978049c319f83ef
moksh chadha <mokshchadha@mokshs-MacBook-Pro.local>
2022-11-06 19:54:47 +0530

    socket io part done

--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:docs/common/widgets/loader.dart';
 import 'package:docs/models/error_model.dart';
 import 'package:docs/repository/auth.dart';
 import 'package:docs/repository/document_repository.dart';
+import 'package:docs/repository/socket_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -17,14 +21,29 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
   final TextEditingController _titleController =
       TextEditingController(text: 'Untitled Document');
 
-  final quill.QuillController _controller = quill.QuillController.basic();
+  quill.QuillController? _controller;
+
+  SocketRepository socketRepository = SocketRepository();
 
   ErrorModel? errorModel;
 
   @override
   void initState() {
     super.initState();
+    socketRepository.joinRoom(widget.id);
     fetchDocumentData();
+    socketRepository.changeListener((data) {
+      _controller?.compose(
+          quill.Delta.fromJson(data['delta']),
+          _controller?.selection ?? const TextSelection.collapsed(offset: 0),
+          quill.ChangeSource.REMOTE);
+    });
+    Timer.periodic(const Duration(seconds: 2), (timer) {
+      socketRepository.autoSave(<String, dynamic>{
+        'delta': _controller!.document.toDelta(),
+        'room': widget.id,
+      });
+    });
   }
 
   void fetchDocumentData() async {
@@ -34,8 +53,23 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
 
     if (errorModel!.data != null) {
       _titleController.text = errorModel!.data.title;
+      _controller = quill.QuillController(
+          selection: const TextSelection.collapsed(offset: 0),
+          document: errorModel!.data.content.isEmpty
+              ? quill.Document()
+              : quill.Document.fromDelta(
+                  quill.Delta.fromJson(errorModel!.data.content)));
       setState(() {});
     }
+    _controller?.document.changes.listen((event) {
+      if (event.item3 == quill.ChangeSource.LOCAL) {
+        Map<String, dynamic> map = {
+          'delta': event.item2,
+          'room': widget.id,
+        };
+        socketRepository.typing(map);
+      }
+    });
   }
 
   void updateTitle(WidgetRef ref, String value) {
@@ -49,22 +83,27 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
   Widget build(BuildContext context) {
     final deviceWidth = MediaQuery.of(context).size.width;
     final deviceHeight = MediaQuery.of(context).size.height;
+    if (_controller == null) {
+      return const Scaffold(
+        body: Loader(),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
         title: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 9),
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 9),
           child: Row(children: [
             const Icon(Icons.article, color: Colors.blue),
-            const SizedBox(width: 10),
+            const SizedBox(width: 5),
             SizedBox(
-              width: 300,
+              width: deviceWidth * 0.4,
               child: TextField(
                 controller: _titleController,
                 decoration: const InputDecoration(
                   border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 10),
+                  contentPadding: EdgeInsets.only(left: 5),
                 ),
                 onSubmitted: (value) => updateTitle(ref, value),
               ),
@@ -81,7 +120,7 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
         ),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(5),
             child: ElevatedButton.icon(
                 onPressed: () {},
                 label: const Text('Share'),
@@ -101,19 +140,21 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
           child: Column(
             children: [
               SizedBox(
-                child: quill.QuillToolbar.basic(controller: _controller),
+                child: quill.QuillToolbar.basic(controller: _controller!),
               ),
               const SizedBox(height: 10),
               SizedBox(
                   width: deviceWidth * 0.7,
-                  height: deviceHeight * 0.7,
+                  height: MediaQuery.of(context).viewInsets.bottom == 0
+                      ? deviceHeight * 0.7
+                      : deviceHeight * .4,
                   child: Card(
                       elevation: 5,
                       color: Colors.white,
                       child: Padding(
                         padding: const EdgeInsets.all(20),
                         child: quill.QuillEditor.basic(
-                          controller: _controller,
+                          controller: _controller!,
                           readOnly: false,
                         ),
                       )))
